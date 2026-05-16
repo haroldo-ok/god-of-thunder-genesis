6d0d3544488e2797906a0602cf535899528b0f21
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2026-05-16 08:03:02 -0300

    Bump version to 0.0.2

--- src/boot/rom_head.c
+++ src/boot/rom_head.c
@@ -9,9 +9,9 @@ const ROMHeader rom_header = {
 #else
     "SEGA MEGA DRIVE ",
 #endif
-    "(C)SGDK 2021    ",
-    "SAMPLE PROGRAM                                  ",
-    "SAMPLE PROGRAM                                  ",
+    "(C)SGDK 2026    ",
+    "GOD OF THUNDER 0.0.2                            ",
+    "GOD OF THUNDER 0.0.2                            ",
     "GM 00000000-00",
     0x000,
     "JD              ",
@@ -28,6 +28,6 @@ const ROMHeader rom_header = {
     0x00200000,
     0x0020FFFF,
     "            ",
-    "DEMONSTRATION PROGRAM                   ",
+    "GOD OF THUNDER 0.0.2                    ",
     "JUE             "
 };
